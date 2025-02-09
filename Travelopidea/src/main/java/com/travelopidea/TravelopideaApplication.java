9864ef9db04cdb0c43802add0dacadff1d4a20d2
Samruddhi362 <82897864+Samruddhi362@users.noreply.github.com>
2025-02-09 15:00:57 +0530

    Backend-travelopidea

---
+++ Travelopidea/src/main/java/com/travelopidea/TravelopideaApplication.java
@@ -0,0 +1,13 @@
+package com.travelopidea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelopideaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelopideaApplication.class, args);
+	}
+
+}
